7ec7bca51e008d2a64278d72976f048e2a224046
Ramanandkushwaha <anand.kushwaha49@gmail.com>
2018-05-30 09:34:21 +0000

    dashboard and routing add

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

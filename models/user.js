53705b1bc3ce7e5d209065b069ea429d0cba5bd7
Ramanandkushwaha <anand.kushwaha49@gmail.com>
2018-05-31 14:32:04 +0000

    email validate

--- models/user.js
+++ models/user.js
@@ -2,8 +2,50 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
+
+// Validate Function to check e-mail length
+let emailLengthChecker = (email) => {
+    // Check if e-mail exists
+    if (!email) {
+      return false; // Return error
+    } else {
+      // Check the length of e-mail string
+      if (email.length < 5 || email.length > 30) {
+        return false; // Return error if not within proper length
+      } else {
+        return true; // Return as valid e-mail
+      }
+    }
+  };
+  
+  // Validate Function to check if valid e-mail format
+  let validEmailChecker = (email) => {
+    // Check if e-mail exists
+    if (!email) {
+      return false; // Return error
+    } else {
+      // Regular expression to test for a valid e-mail
+      const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      return regExp.test(email); // Return regular expression test results (true or false)
+    }
+  };
+  
+  // Array of Email Validators
+  const emailValidators = [
+    // First Email Validator
+    {
+      validator: emailLengthChecker,
+      message: 'E-mail must be at least 5 characters but no more than 30'
+    },
+    // Second Email Validator
+    {
+      validator: validEmailChecker,
+      message: 'Must be a valid e-mail'
+    }
+  ];
+
 const userSchema = new Schema({
-    username:  {type: String, unique: true, lowercase: true, required: true},
+    username:  {type: String, unique: true, lowercase: true, required: true, validate: emailValidators },
     email:     {type: String, unique: true, lowercase: true, required: true},
     password:  {type: String, required: true},
 });
@@ -18,5 +60,9 @@ userSchema.pre('save', function(next) {
         next();
     });
 })
+
+userSchema.methods.comparePassword = (password) => {
+    return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model('user', userSchema);

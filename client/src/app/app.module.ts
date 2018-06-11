398e5ae8e1a376fcfeccecc5f40853a80e794c8a
Ramanandkushwaha <anand.kushwaha49@gmail.com>
2018-06-11 07:24:11 +0000

    form validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,10 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -17,6 +20,8 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
